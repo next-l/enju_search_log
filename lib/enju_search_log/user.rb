65cb7606c6631a307c4db7abdffb843a76d96835
Kosuke Tanabe <tanabe@mwr.mediacom.keio.ac.jp>
2013-04-17 16:27:07 +0900

    fixed logging username

--- lib/enju_search_log/user.rb
+++ lib/enju_search_log/user.rb
@@ -14,21 +14,18 @@ module EnjuSearchLog
     end
 
     module InstanceMethods
-      def save_search_history(query, offset = 0, total = 0, user = nil)
+      def save_search_history(query, offset = 0, total = 0)
         return unless user.try(:save_search_history)
         if Setting.write_search_log_to_file
-          write_search_log(query, total, user)
+          write_search_log(query, total, username)
         else
           history = SearchHistory.new(:query => query, :start_record => offset + 1, :maximum_records => nil, :number_of_records => total)
-          history.user = user
+          history.user = self
           history.save
         end
       end
 
-      def write_search_log(query, total, user)
-        if user
-          username = user.username if user.save_search_history
-        end
+      def write_search_log(query, total, username)
         SEARCH_LOGGER.info "#{Time.zone.now}\t#{query}\t#{total}\t#{username}\t#{params[:format]}"
       end
     end
