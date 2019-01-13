506f2fa23bcd0d9cce1ddf66b97734095905f1ac
Kosuke Tanabe <nabeta@fastmail.fm>
2019-01-14 02:25:29 +0900

    require sunspot_matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,6 +8,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'factory_bot'
+require 'sunspot_matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -34,6 +35,10 @@ RSpec.configure do |config|
   config.extend ControllerMacros, type: :controller
 
   config.infer_spec_type_from_file_location!
+  config.before do
+    Sunspot.session = SunspotMatchers::SunspotSessionSpy.new(Sunspot.session)
+  end
+  config.include SunspotMatchers
 end
 
 FactoryBot.definition_file_paths << "#{::Rails.root}/../../spec/factories"
